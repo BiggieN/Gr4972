1fcccd49c3d9da2e822b5686b33bf8c239718af3
Eugene <gavrilovec.evgen@gmail.com>
2023-09-04 21:48:04 +0300

    First Seminar

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int c = b * b;
+
+if (c == a)
+    {
+        Console.WriteLine("Первое число является квадратом второго");
+    }
+else
+    {
+        Console.WriteLine("Первое число не является квадратом второго");
+    }
