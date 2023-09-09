85702037a0f4efa326f8f78da9bb6fba765cfbcc
Eugene <gavrilovec.evgen@gmail.com>
2023-09-09 11:36:09 +0300

    Home Work 2

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,25 +1,14 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// * Сделать вариант для числа длиной до 10 цифр не используя char или string
 Console.WriteLine("Введите число");
-long number = long.Parse(Console.ReadLine());
+string number = Console.ReadLine();
 
-if (number < 100)
+char[] digitArray = number.ToCharArray();
+
+if (digitArray.Length < 3)
 {
     Console.WriteLine("Число имеет меньше 3 цифр.");
 }
 else
 {
-    //int temp = number / 100;
-    long[] temp = {};
-    int i = 0;
-    do
-    {
-        temp[i] = number % 10;
-        number = number - (number % 10);
-        i++;
-    } while (number == 0);
-    for (int j = 0; j < temp.Length; j++)
-    {
-        Console.WriteLine(temp[j] + " ");
-    }
+        Console.WriteLine("Третья цифра числа = " + digitArray[2]);
 }
--- Sem2Task16/Program.cs
+++ Sem2Task16/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+
+//Переменная для проверки, является ли первое число квадратом второго.
+bool check1 = false;
+//Переменная для проверки, является ли второе число квадратом первого.
+bool check2 = false;
+
+//Проверяем на квадрат
+if (num2 * num2 == num1)
+{
+    check1 = true;
+}
+if (num1 * num1 == num2)
+{
+    check2 = true;
+}
+
+//Проверяем результат проверок на квадрат и выводим результат
+if (check1 == true)
+{
+    Console.WriteLine("Первое число является квадратом второго");   
+} else if (check2 == true)
+{
+    Console.WriteLine("Второе число является квадратом первого");   
+} else
+{
+    Console.WriteLine("Числа не являются квадратами друг друга");
+}
