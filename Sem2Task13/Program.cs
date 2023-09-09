c239be30ade74c14debeccf80acec722f84de369
Eugene <gavrilovec.evgen@gmail.com>
2023-09-09 17:12:14 +0300

    Homework 2 with stars

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,14 +1,34 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//* Сделать вариант для числа длиной до 10 цифр не используя char или string
 Console.WriteLine("Введите число");
-string number = Console.ReadLine();
+long number = long.Parse(Console.ReadLine());
 
-char[] digitArray = number.ToCharArray();
-
-if (digitArray.Length < 3)
+//Проверяем, чтобы было 3 и больше цифры в числе
+if (number < 100)
 {
     Console.WriteLine("Число имеет меньше 3 цифр.");
 }
 else
 {
-        Console.WriteLine("Третья цифра числа = " + digitArray[2]);
+    //Инициализируем массив чисел
+    long[] temp = new long[10];
+    //Инициализируем дополнительные переменные - флаги
+    int i = 1;
+    long j = 100;
+    long k = 10;
+    int thirdNumber = -2;
+    //Раскладываем число на цифры с помощью дополнительных переменных
+    temp[0] = number % 10;
+    number -= number % 10;
+    do
+    {
+        temp[i] = (number % j) / k;
+        number -= (number % j);
+        i++;
+        j *= 10;
+        k *= 10;
+        thirdNumber++;
+    } while (!(number == 0));
+
+    Console.WriteLine("Третья цифра: " + temp[thirdNumber]);
 }
