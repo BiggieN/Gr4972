c41c402251025b6724dadd35c79af1258859cec9
Eugene <gavrilovec.evgen@gmail.com>
2023-09-14 00:14:39 +0300

    Third Seminar

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №18
+//Напишите программу, которая по заданному номеручетверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//По координатам точки определяем номер четверти
+int QuterTest(int quarter)
+{
+    if (quarter == 1) return (100, 100);
+    if (quarter == 2) return (-100, 100);
+    if (quarter == 3) return (-100, -100);
+    if (quarter == 4) return (100, -100);
+    return 0;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+int quarter = ReadData("Введите четверть: ");
+
+int x, y = QuterTest(quarter);
+
+PrintResult("Диапазон возможных координат точек в этой четверти по оси Х от 0 до " + x + " , по оси Y от 0 до " + y);
---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №20
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+double length(int xa, int ya, int xb, int yb)
+{
+    double len = Math.Sqrt(((xb - xa) * (xb - xa)) + ((yb - ya) * (yb - ya)));
+    return len;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int xa = ReadData("Введите координату X первой точки: ");
+int ya = ReadData("Введите координату Y первой точки: ");
+int xb = ReadData("Введите координату X второй точки: ");
+int yb = ReadData("Введите координату Y второй точки: ");
+
+double result = Math.Round(length(xa, ya, xb, yb), 3);
+
+PrintResult("Расстояние равно = " + result);
