7a1ce3a9c90f56b90c27b21f764966be6f40c764
Eugene <gavrilovec.evgen@gmail.com>
2023-09-16 23:58:57 +0300

    saturday work

--- Sem4Task26/Program.cs
+++ Sem4Task26/Program.cs
@@ -1,3 +1,39 @@
 ﻿// Задача №26
 //Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
-Console.WriteLine("Hello, World!");
+
+//Вводим данные
+long ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    long number = long.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void fillTemp(List<long> list, long number)
+{
+    //Инициализируем дополнительные переменные - флаги
+    long j = 100;
+    long k = 10;
+    //Раскладываем число на цифры с помощью дополнительных переменных
+    list.Add(number % 10);
+    number -= number % 10;
+    do
+    {
+        list.Add((number % j) / k);
+        number -= (number % j);
+        j *= 10;
+        k *= 10;
+    } while (!(number == 0));
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+long num = ReadData("Введите число: ");
+List<long> temp = new List<long>();
+
+fillTemp(temp, num);
+
+PrintResult("Количество цифр: " + temp.Count);
