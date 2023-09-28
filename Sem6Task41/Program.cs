05d3bd68b001eedc0b4d04928bf2610d87f2a8da
Eugene <gavrilovec.evgen@gmail.com>
2023-09-28 20:36:00 +0300

    homework 6

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,49 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Вводим данные
+string ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    string numbers = Console.ReadLine();
+    return numbers;
+}
+
+string[] SplitString(string numbers)
+{
+    string[] splitNumbersStr = numbers.Split(',');
+    return splitNumbersStr;
+}
+
+void ConvertToInt(string[] splitNumbersStr, int[] numberArray)
+{
+    for (int i = 0; i < splitNumbersStr.Length; i++)
+    {
+        numberArray[i] = int.Parse(splitNumbersStr[i]);
+    }
+}
+
+int CountPositive(int[] numberArray)
+{
+    int count = 0;
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        if (numberArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+string numbers = ReadData("Введите числа через запятую");
+string[] splitNumbersStr = SplitString(numbers);
+int[] numberArray = new int[splitNumbersStr.Length];
+ConvertToInt(splitNumbersStr, numberArray);
+int count = CountPositive(numberArray);
+PrintResult($"Количество чисел больше 0 = {count}");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,53 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//Вводим данные
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+bool ParallelCheck(double k1, double k2)
+{
+    bool parallel = false;
+    if (k1 == k2)
+    {
+        parallel = true;
+    }
+    return parallel;
+}
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    return x;
+}
+
+double FindY(double k1, double x, double b1)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+double b1 = ReadData("Введите b1");
+double k1 = ReadData("Введите k1");
+double b2 = ReadData("Введите b2");
+double k2 = ReadData("Введите k2");
+
+if (ParallelCheck(k1, k2) == true)
+{
+    PrintResult("Прямые параллельны!");
+    return;
+}
+
+double x = FindX(b1, k1, b2, k2);
+double y = FindY(k1, x, b1);
+
+PrintResult($"Точка пересечения двух прямых [{x}, {y}]");
