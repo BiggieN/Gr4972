5e485c6c6c2ca49f56bd48ba95baef299c2850d9
Eugene <gavrilovec.evgen@gmail.com>
2023-10-01 21:15:10 +0300

    Homework 8

---
+++ Sem8Task58/Program.cs
@@ -0,0 +1,153 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Заполняем двумерный массив
+int[,] Fill2DArray(int countRow, int countColumn, int downBorder, int topBorder)
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(downBorder, topBorder);
+        }
+    }
+    return array2D;
+}
+
+// Проверяем на согласованность матриц
+bool IfMatchedMatrix(int rowA, int columnB)
+{
+    if (rowA == columnB)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int[] GetRow(int[,] firstArr2D, int row, int columnA)
+{
+    int[] tempRow = new int[columnA];
+    for (int i = row; i <= row; i++)
+    {
+        for (int j = 0; j < columnA; j++)
+        {
+            tempRow[j] = firstArr2D[i, j];
+        }    
+    }
+    return tempRow;
+}
+
+int[] GetColumn(int[,] secondArr2D, int column, int rowB)
+{
+    int[] tempColumn = new int[rowB];
+    for (int i = column; i <= column; i++)
+    {
+        for (int j = 0; j < rowB; j++)
+        {
+            tempColumn[j] = secondArr2D[j, i];
+        }
+    }
+    return tempColumn;
+}
+
+int MultipleElement(int[] tempRow, int[] tempColumn)
+{
+    int element = 0;
+    for (int i = 0; i < tempRow.Length; i++)
+    {
+        int tempMultiple = 0;
+        tempMultiple = tempRow[i] * tempColumn[i];
+        element += tempMultiple;
+    }
+    return element;
+}
+
+int[,] MultipleMatrix(int[,] firstArr2D, int[,] secondArr2D, int rowA, int columnA, int rowB, int columnB)
+{
+    int[,] multipleMatrix = new int[rowA, columnB];
+    for (int i = 0; i < rowA; i++)
+    {
+        for (int j = 0; j < columnB; j++)
+        {
+            multipleMatrix[i, j] = MultipleElement(GetRow(firstArr2D, i, columnA), GetColumn(secondArr2D, j, rowB));
+        }
+    }
+    return multipleMatrix;
+}
+
+// Печатаем результат
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Печатаем матрицу сумм
+void PrintSummArray(int[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        Console.Write(matrix[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Находим строку с наименьшей суммой
+int SmallestRaw(int[] summArray)
+{
+    int temp = 0;
+    for (int i = 1; i < summArray.Length; i++)
+    {
+        if (summArray[temp] > summArray[i])
+        {
+            temp = i;
+        }
+    }
+    return temp;
+}
+
+int rowA = ReadData("Введите количество строк первой матрицы: ");
+int columnA = ReadData("Введите количество столбцов первой матрицы: ");
+int rowB = ReadData("Введите количество строк второй матрицы: ");
+int columnB = ReadData("Введите количество столбцов второй матрицы: ");
+if (IfMatchedMatrix(rowA, columnB))
+{
+    int[,] firstArr2D = Fill2DArray(rowA, columnA, 1, 11);
+    int[,] secondArr2D = Fill2DArray(rowB, columnB, 1, 11);
+    Print2DArray(firstArr2D);
+    Console.WriteLine();
+    Print2DArray(secondArr2D);
+    Console.WriteLine();
+    Print2DArray(MultipleMatrix(firstArr2D, secondArr2D, rowA, columnA, rowB, columnB));
+    //PrintSummArray(CountLineElementsSumm(arr2D));
+    //PrintResult($"Строка, с наименьшей суммой элементов {SmallestRaw(CountLineElementsSumm(arr2D)) + 1}");
+}
+else
+{
+    PrintResult("Перемножать можно только согласованные матрицы");
+}
---
+++ Sem8Task60/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Заполняем двумерный массив
+int[,,] Fill3DArray(int x, int y, int z, int downBorder, int topBorder)
+{
+    System.Random rnd = new System.Random();
+    int[,,] array3D = new int[z, y, z];
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                int temp = rnd.Next(10, 100);
+
+                while (RepeatedElement(array3D, temp))
+                {
+                    temp = rnd.Next(10, 100);
+                }
+
+                array3D[i, j, k] = temp;
+            }
+        }
+    }
+    return array3D;
+}
+
+bool RepeatedElement(int[,,] array3D, int temp)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                if (temp == array3D[i, j, k])
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+// Печатаем результат
+void Print3DArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"[{i},{j},{k}] {matrix[i, j, k]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int x = ReadData("Введите первую размерность матрицы: ");
+int y = ReadData("Введите вторую размерность матрицы: ");
+int z = ReadData("Введите третью размерность матрицы: ");
+int[,,] array3D = Fill3DArray(x, y, z, 10, 100);
+Print3DArray(array3D);
