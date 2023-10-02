8ca0a801275f05a54b461c9d27d8015c816eacfe
Eugene <gavrilovec.evgen@gmail.com>
2023-10-02 23:49:48 +0300

    Homework9 Part1

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Формирование строки с числами
+string SearchForNaturalElementsRec (int m, int n)
+{
+    return n >= m ? $"{m} " + SearchForNaturalElementsRec(m + 1, n) : "";
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+
+Console.WriteLine((m>n)?SearchForNaturalElementsRec(n, m):SearchForNaturalElementsRec(m, n));*/
+
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int SumNaturalRec(int m, int n)
+{
+    if (m < n)
+    {
+        return m + SumNaturalRec(m, n + 1);
+    }
+    return 0;
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+Console.WriteLine((m>n)?SumNaturalRec(n, m):SumNaturalRec(m, n));
